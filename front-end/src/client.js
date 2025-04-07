a9d588c207b78703e807c3819c2b5ad7c3f32d66
vjamespd <dvinardjames@gmail.com>
2025-04-07 15:10:45 +0800

    hardcoded token

--- front-end/src/client.js
+++ front-end/src/client.js
@@ -5,7 +5,7 @@ export const client = createClient({
     projectId: import.meta.env.VITE_PROJECT_ID,
     dataset: "production",
     apiVersion: "2025-02-06",
-    token: import.meta.env.VITE_PROJECT_TOKEN,
+    token: "skTo5F1QsXgQWT9L6LJC7lED3DASbIKoZv2nNfa77dU4wcjKdl4N1vpWcR0hwfFzFyMFpN9ustfcXUSqJI0UzYKNf9qnDbGFohNf3SPy4Z6W1SmACxZs07PjUK1n5hc7Ru7bYGgSGjYWVQGTtLSWSx6zulZDHLvct5dxzFfkHX9zxibG1soK",
     useCdn: false,
     ignoreBrowserTokenWarning:true
 });
